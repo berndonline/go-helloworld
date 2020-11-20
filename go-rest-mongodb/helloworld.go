9b163c47c430e826d31d9b379f6846c9d455b62d
Bernd Malmqvist <bernd@lehmann.be>
2020-11-20 20:44:17 +0000

    update mongodb structure

--- go-rest-mongodb/helloworld.go
+++ go-rest-mongodb/helloworld.go
@@ -22,8 +22,8 @@ var database = os.Getenv("DATABASE")
 
 func BasicAuth(handler http.HandlerFunc, realm string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		user, pass, ok := r.BasicAuth()
-		if !ok || subtle.ConstantTimeCompare([]byte(user),
+		Content, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(Content),
 			[]byte(ADMIN_USER)) != 1 || subtle.ConstantTimeCompare([]byte(pass),
 			[]byte(ADMIN_PASSWORD)) != 1 {
 			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
@@ -47,25 +47,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, response+"\n"+os.Getenv("HOSTNAME"))
 }
 
-type User struct {
+type Content struct {
 	ID bson.ObjectId `bson:"_id" json:"id"`
 	Name string `bson:"name" json:"name"`
 }
 
-type UsersDAO struct {
+type ContentsDAO struct {
 	Server   string
 	Database string
 }
 
-var dao = UsersDAO{}
+var dao = ContentsDAO{}
 var db *mgo.Database
 
 const (
-	COLLECTION = "users"
+	COLLECTION = "Contents"
 )
 
 // Establish a connection to database
-func (m *UsersDAO) Connect() {
+func (m *ContentsDAO) Connect() {
 	session, err := mgo.Dial(m.Server)
 	if err != nil {
 		log.Fatal(err)
@@ -73,103 +73,103 @@ func (m *UsersDAO) Connect() {
 	db = session.DB(m.Database)
 }
 
-// Find list of users
-func (m *UsersDAO) FindAll() ([]User, error) {
-	var users []User
-	err := db.C(COLLECTION).Find(bson.M{}).All(&users)
-	return users, err
+// Find list of Contents
+func (m *ContentsDAO) FindAll() ([]Content, error) {
+	var Contents []Content
+	err := db.C(COLLECTION).Find(bson.M{}).All(&Contents)
+	return Contents, err
 }
 
-// Find a user by its id
-func (m *UsersDAO) FindById(id string) (User, error) {
-	var user User
-	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&user)
-	return user, err
+// Find a Content by its id
+func (m *ContentsDAO) FindById(id string) (Content, error) {
+	var Content Content
+	err := db.C(COLLECTION).FindId(bson.ObjectIdHex(id)).One(&Content)
+	return Content, err
 }
 
-// Insert a user into database
-func (m *UsersDAO) Insert(user User) error {
-	err := db.C(COLLECTION).Insert(&user)
+// Insert a Content into database
+func (m *ContentsDAO) Insert(Content Content) error {
+	err := db.C(COLLECTION).Insert(&Content)
 	return err
 }
 
-// Delete an existing user
-func (m *UsersDAO) Delete(user User) error {
-	err := db.C(COLLECTION).Remove(&user)
+// Delete an existing Content
+func (m *ContentsDAO) Delete(Content Content) error {
+	err := db.C(COLLECTION).Remove(&Content)
 	return err
 }
 
-// Update an existing user
-func (m *UsersDAO) Update(user User) error {
-	err := db.C(COLLECTION).UpdateId(user.ID, &user)
+// Update an existing Content
+func (m *ContentsDAO) Update(Content Content) error {
+	err := db.C(COLLECTION).UpdateId(Content.ID, &Content)
 	return err
 }
 
-// GET list of users
-func AllUsersEndPoint(w http.ResponseWriter, r *http.Request) {
-	users, err := dao.FindAll()
+// GET list of Contents
+func AllContentsEndPoint(w http.ResponseWriter, r *http.Request) {
+	Contents, err := dao.FindAll()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, users)
+	respondWithJson(w, http.StatusOK, Contents)
 }
 
-// GET a users by its ID
-func FindUserEndpoint(w http.ResponseWriter, r *http.Request) {
+// GET a Contents by its ID
+func FindContentEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	user, err := dao.FindById(params["id"])
+	Content, err := dao.FindById(params["id"])
 	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		respondWithError(w, http.StatusBadRequest, "Invalid Content ID")
 		log.Print(err)
 		return
 	}
-	respondWithJson(w, http.StatusOK, user)
+	respondWithJson(w, http.StatusOK, Content)
 }
 
-// POST a new user
-func CreateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+// POST a new Content
+func CreateContentEndPoint(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	var Content Content
+	if err := json.NewDecoder(r.Body).Decode(&Content); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		log.Print(err)
 		return
 	}
-	user.ID = bson.NewObjectId()
-	if err := dao.Insert(user); err != nil {
+	Content.ID = bson.NewObjectId()
+	if err := dao.Insert(Content); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusCreated, user)
+	respondWithJson(w, http.StatusCreated, Content)
 }
 
-// PUT update an existing user
-func UpdateUserEndPoint(w http.ResponseWriter, r *http.Request) {
+// PUT update an existing Content
+func UpdateContentEndPoint(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	params := mux.Vars(r)
-	var user User
-	user.ID = bson.ObjectIdHex(params["id"])
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	var Content Content
+	Content.ID = bson.ObjectIdHex(params["id"])
+	if err := json.NewDecoder(r.Body).Decode(&Content); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
-	if err := dao.Update(user); err != nil {
+	if err := dao.Update(Content); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
-// DELETE an existing user
-func DeleteUserEndPoint(w http.ResponseWriter, r *http.Request) {
+// DELETE an existing Content
+func DeleteContentEndPoint(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	var Content Content
+	if err := json.NewDecoder(r.Body).Decode(&Content); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
-	if err := dao.Delete(user); err != nil {
+	if err := dao.Delete(Content); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
@@ -206,11 +206,11 @@ func main() {
 	log.Print("helloworld: is starting...")
 	r := mux.NewRouter().StrictSlash(true)
 	r.HandleFunc("/", handler)
-	r.HandleFunc("/api/v1/content", BasicAuth(AllUsersEndPoint, "Please enter your username and password")).Methods("GET")
-	r.HandleFunc("/api/v1/content", BasicAuth(CreateUserEndPoint, "Please enter your username and password")).Methods("POST")
-	r.HandleFunc("/api/v1/content", BasicAuth(DeleteUserEndPoint, "Please enter your username and password")).Methods("DELETE")
-	r.HandleFunc("/api/v1/content/{id}", BasicAuth(UpdateUserEndPoint, "Please enter your username and password")).Methods("PUT")
-	r.HandleFunc("/api/v1/content/{id}", BasicAuth(FindUserEndpoint, "Please enter your username and password")).Methods("GET")
+	r.HandleFunc("/api/v1/content", BasicAuth(AllContentsEndPoint, "Please enter your username and password")).Methods("GET")
+	r.HandleFunc("/api/v1/content", BasicAuth(CreateContentEndPoint, "Please enter your username and password")).Methods("POST")
+	r.HandleFunc("/api/v1/content", BasicAuth(DeleteContentEndPoint, "Please enter your username and password")).Methods("DELETE")
+	r.HandleFunc("/api/v1/content/{id}", BasicAuth(UpdateContentEndPoint, "Please enter your username and password")).Methods("PUT")
+	r.HandleFunc("/api/v1/content/{id}", BasicAuth(FindContentEndpoint, "Please enter your username and password")).Methods("GET")
 	port := os.Getenv("PORT")
 
 	if port == "" {
