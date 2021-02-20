8fa0f0af9e05b59bfa8b2e5582497e267e941be0
Bernd Malmqvist <bernd@lehmann.be>
2021-02-20 13:36:29 +0000

    Update prometheus_test.go

--- prometheus_test.go
+++ prometheus_test.go
@@ -13,13 +13,13 @@ import (
 
 func Test_Standard_InstrumentHandler(t *testing.T) {
 
-  r := prometheus.NewRegistry()
-  r.MustRegister(httpRequestDuration)
-  r.MustRegister(httpRequestsTotal)
-  r.MustRegister(httpRequestsResponseTime)
-  r.MustRegister(httpRequestSizeBytes)
-  r.MustRegister(httpResponseSizeBytes)
-  r.MustRegister(version)
+	r := prometheus.NewRegistry()
+	r.MustRegister(httpRequestDuration)
+	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(httpRequestsResponseTime)
+	r.MustRegister(httpRequestSizeBytes)
+	r.MustRegister(httpResponseSizeBytes)
+	r.MustRegister(version)
 
 	router := mux.NewRouter()
 	router.Path("/metrics").Handler(promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
@@ -41,29 +41,29 @@ func Test_Standard_InstrumentHandler(t *testing.T) {
 	router.ServeHTTP(rr, req2)
 	body := rr.Body.String()
 
-  t.Run("Check http_request_duration_seconds", func(t *testing.T) {
-  	if !strings.Contains(body, "http_request_duration_seconds") {
-  		t.Errorf("body does not contain request total entry '%s'", "http_request_duration_seconds")
-  	}
-  })
-  t.Run("Check http_requests_total", func(t *testing.T) {
-  	if !strings.Contains(body, "http_requests_total") {
-  		t.Errorf("body does not contain request duration entry '%s'", "http_requests_total")
-  	}
-  })
-  t.Run("Check http_request_size_bytes", func(t *testing.T) {
-    if !strings.Contains(body, "http_request_size_bytes") {
-  		t.Errorf("body does not contain request total entry '%s'", "http_request_size_bytes")
-  	}
-  })
-  t.Run("Check http_response_size_bytes", func(t *testing.T) {
-  	if !strings.Contains(body, "http_response_size_bytes") {
-  		t.Errorf("body does not contain request duration entry '%s'", "http_response_size_bytes")
-  	}
-  })
-  t.Run("Check version", func(t *testing.T) {
-    if !strings.Contains(body, "version") {
-      t.Errorf("body does not contain request duration entry '%s'", "versions")
-    }
-  })
+	t.Run("Check http_request_duration_seconds", func(t *testing.T) {
+	  if !strings.Contains(body, "http_request_duration_seconds") {
+	  	t.Errorf("body does not contain request total entry '%s'", "http_request_duration_seconds")
+	  }
+	})
+	t.Run("Check http_requests_total", func(t *testing.T) {
+	  if !strings.Contains(body, "http_requests_total") {
+	  	t.Errorf("body does not contain request duration entry '%s'", "http_requests_total")
+	  }
+	})
+	t.Run("Check http_request_size_bytes", func(t *testing.T) {
+	  if !strings.Contains(body, "http_request_size_bytes") {
+			t.Errorf("body does not contain request total entry '%s'", "http_request_size_bytes")
+		}
+	})
+	t.Run("Check http_response_size_bytes", func(t *testing.T) {
+		if !strings.Contains(body, "http_response_size_bytes") {
+			t.Errorf("body does not contain request duration entry '%s'", "http_response_size_bytes")
+		}
+	})
+	t.Run("Check version", func(t *testing.T) {
+	  if !strings.Contains(body, "version") {
+	    t.Errorf("body does not contain request duration entry '%s'", "versions")
+	  }
+	})
 }
